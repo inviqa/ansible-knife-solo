2c679e24d8d86907195cadeef1843147140db5cd
Kieren Evans <kevans@inviqa.com>
2018-05-11 21:31:29 +0300

    Turn off local_mode for knife solo to work

--- tests/chef/knife.rb
+++ tests/chef/knife.rb
@@ -2,5 +2,5 @@ cookbook_path [ "./site-cookbooks" ]
 role_path "./roles"
 # data_bag_path "./data_bags"
 environment_path "./environments"
-local_mode true
+# local_mode true
 # knife[:secret_file] = "./encrypted_data_bag_secret"
